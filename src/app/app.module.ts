8871e5658bd23ffdbb762ed7e36767e80235cdb1
jordiascension <jordiascension@enterpriseconsulting.es>
2023-11-17 00:46:09 +0100

    Added Angular Material closes #7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
-import { CalculadoraDesgravacioComponent } from './calculadora-desgravacio/calculadora-desgravacio.component';
-
 import { ReactiveFormsModule } from '@angular/forms';
+import { PensionPlanService } from './pension-plan.service';
+import { CalculadoraDesgravacioComponent } from './calculadora-desgravacio/calculadora-desgravacio.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule} from '@angular/material/button';
+import { MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioComponent,
     CalculadoraDesgravacioComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [PensionPlanService],
   bootstrap: [CalculadoraDesgravacioComponent]
 })
 export class AppModule { }
